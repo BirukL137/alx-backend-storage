82bbb956d2d49bc192541f25d62b58f473f9e7b1
BirukL137 <buraf13k@gmail.com>
2023-04-06 14:20:14 +0300

    11. No table for a meeting

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,3 +1,6 @@
+-- This script creates a view need_meeting and lists all students
+-- that have a score under 80 (strict) and no last_meeting or more than 1 month.
+
 CREATE VIEW need_meeting AS
 SELECT name
 FROM students
