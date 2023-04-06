671e21588079ed7a13828c965d5a94778256e89e
BirukL137 <buraf13k@gmail.com>
2023-04-06 14:34:45 +0300

    12. Average weighted score

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -10,7 +10,7 @@ BEGIN
 	JOIN corrrections as COR ON US.id=COR.user_id
 	JOIN projects AS PRO ON COR.project_id=PRO.id WHERE US.id=user_id);
 
-    UPDATE users SET weighted_score = avg_score WHERE id = user_id;
+    UPDATE users SET average_score = avg_score WHERE id = user_id;
 END//
 
 DELIMITER ;
