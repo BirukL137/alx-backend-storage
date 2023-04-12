509cfebdbb3dc96653f0f72c3b6f003d248fd8c2
BirukL137 <buraf13k@gmail.com>
2023-04-12 19:48:34 +0300

    5. Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -10,18 +10,32 @@ from time import time
 cache = {}
 
 
+def cache_decorator(func):
+    """
+    cache decorator function
+    """
+    def wrapper(url):
+        """
+        This function will track how many times a particular URL
+        was accessed in the key "count:{url}" and cache the result
+        with an expiration time of 10 seconds.
+        """
+        
+        if url in cache and time() - cache[url]['time'] < 10:
+            cache[url]['count'] += 1
+            return cache[url]['content']
+        else:
+            response = requests.get(url)
+            content = response.content
+            cache[url] = {'content': content, 'count': 1, 'time': time()}
+            return content
+        return wrapper
+
+@cache_decorator
 def get_page(url: str) -> str:
     """
-    This function will track how many times a particular URL
-    was accessed in the key "count:{url}" and cache the result
-    with an expiration time of 10 seconds.
+    Returns response content 
     """
 
-    if url in cache and time() - cache[url]['time'] < 10:
-        cache[url]['count'] += 1
-        return cache[url]['content']
-    else:
-        response = requests.get(url)
-        content = response.content
-        cache[url] = {'content': content, 'count': 1, 'time': time()}
-        return content
+    response = requests.get(url)
+    return response.content
